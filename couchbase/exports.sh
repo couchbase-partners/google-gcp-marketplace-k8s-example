5bd9e7fd8f693c89e52ba6dceeb866da5fd8d425
Shomari Liburd <shomari.liburd@couchbase.com>
2019-04-05 00:44:32 -0700

    - Removed duplicate files - Updated names to match their position in the order of commands - Add a pillowfight that isn't working yet, but is almost there. - Added a crd - Updated files for operator 1.1, including exports.sh, Makefile and schemas - Updated the manifest file cb_operator_application.yaml with many changes needed for operator 1.1. - Added Persistent Volumes and the Claim

--- couchbase/exports.sh
+++ couchbase/exports.sh
@@ -6,21 +6,28 @@ export NAME=cb-operator-1
 #REGISTRY has to match your target GCP project
 export REGISTRY=gcr.io/couchbase-dev
 export IMAGE_BASE=/k8s/operator
-export TAG=1.0
+export TAG=2.0
 export CONTAINER_PORT=8080
 export DB_PASSWORD=longPass32d
 export DB_USERNAME=couchbaseAdmin
 export INIT_DELAY_SEC=30
 export PERIOD_SEC=5
 export FAIL_THRESHOLD=19
-export OP_REPLICAS=1
+#export OP_REPLICAS=1
 export CLUSTER_REPLICAS=1
 export DATA_SVC_QUOTA=256
 export IDX_SVC_QUOTA=256
 export SRC_SVC_QUOTA=256
+export EVT_SVC_QUOTA=256  #new
+export ANA_SVC_QUOTA=1024 #new
 export SERVERS=3
 export BUCKET_MEM_QUOTA=256
-export AUTO_FAIL_TIMEOUT=5
+export AUTO_FAIL_TIMEOUT=120
+export AUTO_FAIL_MAX=3 #new
+export AUTO_FAIL_ODDI=true #new boolean
+export AUTO_FAIL_ODDI_TP=120  #new
+export AUTO_FAIL_SG=false #new boolean
+export STORAGE_CLASS=standard #new 
 
 #######DO NOT EDIT BELOW!
 
@@ -28,5 +35,4 @@ export SECRET_NAME=$NAME-secret
 export CB_OP_IMAGE=$REGISTRY$IMAGE_BASE:$TAG
 export CB_DEPLOYER_IMAGE=$REGISTRY$IMAGE_BASE/deployer:$TAG
 export DB_PASSWORD=`echo -n $DB_PASSWORD | base64`
-#export DB_USERNAME=`echo -n $DB_USERNAME | base64`
 
